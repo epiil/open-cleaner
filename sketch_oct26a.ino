9ccec3112dce6217d23f5623d4522bf119fc4b1b
epiil <piil.erik@gmail.com>
2014-10-14 00:50:14 -0400

    motor and servo proper names

--- sketch_oct26a.ino
+++ sketch_oct26a.ino
@@ -1,79 +1,61 @@
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
-Servo myservo2;  // create servo object to control a servo
+Servo pellonservo1;  // create pellonservo1 object 
+Servo pellonservo2;  // create pellonservo2 object
                 
-int pos = 0;    // variable to store the servo position 
+int pos = 0;    // variable to store servo position 
 
 #include <AFMotor.h>
 
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
-AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
+AF_DCMotor motorsupply(1);
+AF_DCMotor motortakeup(2);
  
 void setup() 
 { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-  myservo2.attach(10);  // attaches the servo on pin 9 to the servo object
+  pellonservo1.attach(9);  // attaches the servo on pin 9 to the servo object 
+  pellonservo2.attach(10);  // attaches the servo on pin 9 to the servo object
   for(pos = 0; pos < 1; pos += 1)  // goes from 0 degrees to 1 degrees 
   {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    myservo2.write(pos);              // tell servo to go to position in variable 'pos' 
+    pellonservo1.write(pos);              // tell servo to go to position in variable 'pos'
+    pellonservo2.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(100);                       // waits 15ms for the servo to reach the position 
   } 
    Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor 1, 2, 3, 4 test");
-    motor1.setSpeed(1000);
-    motor2.setSpeed(1000);
-    motor3.setSpeed(1000);
-    motor4.setSpeed(1000);
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  motor3.run(RELEASE);
-  motor4.run(RELEASE);
+    motorsupply.setSpeed(1000);
+    motortakeup.setSpeed(1000);
+  motorsupply.run(RELEASE);
+  motortakeup.run(RELEASE);
 }
 
 int i;
 
 void loop() {
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
+  motorsupply.run(FORWARD);
+  motortakeup.run(FORWARD);
   for (i=0; i<255; i++) {
-    motor1.setSpeed(i); 
-    motor2.setSpeed(i); 
-    motor3.setSpeed(i);
-    motor4.setSpeed(1);
+    motorsupply.setSpeed(i); 
+    motortakeup.setSpeed(i); 
     delay(70);
  }
  
   for (i=255; i!=0; i--) {
-    motor1.setSpeed(i);
-    motor2.setSpeed(i);  
-    motor3.setSpeed(i);
-    motor4.setSpeed(1);
+    motorsupply.setSpeed(i);
+    motortakeup.setSpeed(i);  
     delay(70);
  }
  
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);
+  motorsupply.run(BACKWARD);
+  motortakeup.run(BACKWARD);
   for (i=0; i<255; i++) {
-    motor1.setSpeed(i); 
+    motorsupply.setSpeed(i); 
     motor2.setSpeed(i); 
-    motor3.setSpeed(i);
-    motor4.setSpeed(i);
     delay(70);
  }
  
   for (i=255; i!=0; i--) {
-    motor1.setSpeed(i); 
-    motor2.setSpeed(i); 
-    motor3.setSpeed(i);
-    motor4.setSpeed(i);
+    motorsupply.setSpeed(i); 
+    motortakeup.setSpeed(i); 
     delay(70);
  }
 }
