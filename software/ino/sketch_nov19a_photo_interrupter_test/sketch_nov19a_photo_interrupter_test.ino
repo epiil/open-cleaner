a74025049219014fbf58e18f14b4d64f11a32c69
Erik Piil <piil.erik@gmail.com>
2014-11-19 23:13:51 -0500

    added photo interrupter sketch test

---
+++ software/ino/sketch_nov19a_photo_interrupter_test/sketch_nov19a_photo_interrupter_test.ino
@@ -0,0 +1,14 @@
+int sensorPin = 0;
+int ledPin = 13;
+int sensorValue = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("start"); 
+}
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  digitalWrite(ledPin, sensorValue);
+  Serial.println("transistor activating");
+}
