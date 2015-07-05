75629f3749a754cb61a8c04436d79955a3f9204b
Erik Piil <piil.erik@gmail.com>
2015-07-05 13:45:22 -0400

    roller guide update

--- hardware/4_tape_cleaner_roller/scad/tape_path_roller.scad
+++ hardware/4_tape_cleaner_roller/scad/tape_path_roller.scad
@@ -1,3 +1,9 @@
+//Opencleaner - Precision Roller Guide
+//Revised 5 Jul 2015
+//Designed for 1/4" shaft
+//in milimeters, inches are preferred
+
+
 $fn=100;
 
 module roller() {
@@ -7,19 +13,21 @@ module roller() {
 		union() 
 		color("black")
 		{
-		cylinder(h=21.08, d=21.20, $fs=.01); //base
-		cylinder(r1=21.20, r2=15, h=2.36, $fs=.01);
-		translate([0,0,19.9])
-		cylinder(r1=15, r2=21.20, h=2.36, $fs=.01);
+		cylinder(h=20.828, d=21.20, $fs=.01); // .820" total width .80 Inner width + 
+		cylinder(r=21.20, h=2.54, $fs=.01); //bottom flange, .1"
+		translate([0,0,20.574]) //.810" height to top flange
+		cylinder(r=21.20, h=2.54, $fs=.01); //top flange, .1"
 		}
 		union()
 		color("black")
 		{	
-		cylinder(h=80, d=11, center = true, $fs=.01);
-		cylinder(h=5.1, d=16, $fs=.01);
-		cylinder(h=2, d=16, center=true, $fs=.01);
-		translate([0,0,17.2])
-		cylinder(h=5.2, d=16, $fs=.01);
+		cylinder(h=80, d=12.7, center = true, $fs=.01); //shaft dimension, 1/2" inch
+		cylinder(h=7.239, d=19.0246, $fs=.01); //bottom bearing offset, .285" wide, .749" diameter
+		cylinder(h=2, d=19.05, center=true, $fs=.01); // overcuts bottom bearing offset
+		translate([0,0,13.589]) //.535" height to bearing offset
+		cylinder(h=7.239, d=19.0246, $fs=.01); //top bearing offset, .285" wide, .749" diameter
+		translate([0,0,20]) //overcuts top bearing offset
+		cylinder(h=7.239, d=19.0246, $fs=.01); //top bearing offset, .285" wide, .749" diameter
 		}
 	}
 }
--- software/Adafruit_Motor_Shield_V2_Library/examples/DCMotorTest/DCMotorTest.ino
+++ software/Adafruit_Motor_Shield_V2_Library/examples/DCMotorTest/DCMotorTest.ino
@@ -1,23 +1,15 @@
-/* 
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
-
-For use with the Adafruit Motor Shield v2 
----->	http://www.adafruit.com/products/1438
-*/
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+//Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 // Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
 // You can also make another motor on port M2
 //Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
@@ -29,7 +21,7 @@ void setup() {
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(150);
+  myMotor->setSpeed(0);
   myMotor->run(FORWARD);
   // turn on motor
   myMotor->run(RELEASE);
@@ -43,26 +35,5 @@ void loop() {
   myMotor->run(FORWARD);
   for (i=0; i<255; i++) {
     myMotor->setSpeed(i);  
-    delay(10);
   }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  
-  Serial.print("tock");
-
-  myMotor->run(BACKWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-
-  Serial.print("tech");
-  myMotor->run(RELEASE);
-  delay(1000);
 }
